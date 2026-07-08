ad804153096ec8bc738cf980a877d5791f1cee9f
Pavel Lizunov <slovnmi@gmail.com>
2026-07-08 13:25:58 +0300

    chore(release): prepare v2.46.0-r28

--- VPNRouter.Core/AppVersion.cs
+++ VPNRouter.Core/AppVersion.cs
@@ -28,5 +28,5 @@ namespace VPNRouter.Core;
 /// </summary>
 public static class AppVersion
 {
-    public static readonly string Version = "2.46.0-r27";
+    public static readonly string Version = "2.46.0-r28";
 }
